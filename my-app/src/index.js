0a6568b311eae16ff5f6e0bcb86d14958895e970
Steven Huynh <huynhtanhoa2012@gmail.com>
2024-09-25 23:35:43 +0700

    add new file

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function BookList(){
+    return (
+        <section>
+            <Book />
+            <Book />
+            <Book />
+            <Book />
+        </section>
+    );
+}
+
+function Book(){
+    return (
+        <article>
+            <Image />
+            <Title />
+            <Author />
+        </article>
+    );
+}
+
+const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/71wyLqCorxL._AC_UL900_SR900,600_.jpg"
+                         alt="Truths: The Future of America First"/>
+const Title = () => {
+    return <h2>Truths: The Future of America First</h2>
+}
+const Author = () => <h4>Vivek Ramaswamy</h4>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<BookList />);

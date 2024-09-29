229b393c68b9d7fc16f2007c27e898b1c940ed24
Steven Huynh <huynhtanhoa2012@gmail.com>
2024-09-29 10:16:54 +0700

    modify the structure

---
+++ pdas/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// step 1
+const el = document.getElementById('root');
+
+// step 2
+const root = ReactDOM.createRoot(el);
+
+// step 3
+root.render(<App/>);

1688e5f4f00129536ca0b60e7eeb6fd8f67de51c
Steven Huynh <huynhtanhoa2012@gmail.com>
2024-10-26 19:00:14 +0700

    update BookCreate function

---
+++ books/src/App.js
@@ -0,0 +1,24 @@
+import {useState} from 'react';
+import BookCreate from "./components/BookCreate";
+
+function App() {
+    const [books, setBooks] = useState([]);
+
+    const createBook = (title) => {
+        const updateBooks = [
+            ...books,
+            {
+                id: Math.round(Math.random() * 9999),
+                title: title}
+        ];
+
+        setBooks(updateBooks);
+    };
+
+    return <div>
+        {books.length}
+        <BookCreate onCreate={createBook}/>
+    </div>;
+}
+
+export default App;

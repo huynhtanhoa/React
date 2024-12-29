62dd4a269d3f66ba2d4945f872614e4e6a30cd2e
Steven Huynh <huynhtanhoa2012@gmail.com>
2024-12-29 12:26:10 +0700

    add all

---
+++ pdas/src/index.js
@@ -0,0 +1,13 @@
+// step 1
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from "./App";
+
+
+// step 2
+const el = document.getElementById("root");
+const root = ReactDOM.createRoot(el);
+
+root.render(<App />);
+
+

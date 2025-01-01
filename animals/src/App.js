3ae3163c0d251e5df689747c46b80d7c2e08feb4
Hoa Huynh <hhuynh@ilts.com>
2025-01-01 13:15:26 +0700

    update animal

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,12 +1,31 @@
 import AnimalShow from "./AnimalShow";
+import {useState} from "react";
+
+function getRandomAnimal(){
+    const animals = ['bird','cat','cow','dog','gator','horse'];
+    return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App(){
 
-  return (
-      <div>
-        <AnimalShow />
-      </div>
-  );
+    const [animals, setAnimals] = useState([]);
+
+    function handleClick(){
+        setAnimals([...animals, getRandomAnimal()]);
+    }
+
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />;
+    })
+
+    return (
+        <div>
+            <div>
+                <button onClick={handleClick}>Add Animal</button>
+            </div>
+            <div>{renderedAnimals}</div>
+        </div>
+    );
 }
 
 export default App;
